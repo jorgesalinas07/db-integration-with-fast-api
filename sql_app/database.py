a7a3cb796dbb9e09ed72d11fabf1f81be4d70188
Jorge Salinas <ext.jsalinas@warbyparker.com>
2023-04-30 17:09:25 -0500

    feat: Set up database

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,12 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
+import os
+from os.path import dirname, join
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), ".env")
 
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+db_username = os.environ.get("DB_USERNAME", "user_test")
+db_pass = os.environ.get("DB_PASS", "postgres")
+db_name = os.environ.get("DB_NAME", "test_db")
+db_port = os.environ.get("DB_PORT", "1234")
+db_host = os.environ.get("DB_HOST", "localhost")
+
+db_string = "postgresql://"+db_username+":"+db_pass+"@"+db_host+":"+db_port+"/"+db_name
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    db_string
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- sql_app/models.py
+++ sql_app/models.py
@@ -1,14 +1,20 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
+# Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
+    email_2 = Column(String, unique=True, index=True)
+    email_3 = Column(String, unique=True, index=True)
+    email_4 = Column(String, unique=True, index=True)
+    email_5 = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
@@ -21,6 +27,9 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
+    description_2 = Column(String, index=True)
+    description_3 = Column(String, index=True)
+    description_4 = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="items")
